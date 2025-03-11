7904a97833113fa5274bf9b0a02e7c4b68def93b
Dio Risso <helenamarxr@gmail.com>
2025-03-11 15:03:33 -0300

    initial commit

---
+++ server/bloggdUp/src/main/java/com/meddle/bloggdUp/BloggdUpApplication.java
@@ -0,0 +1,13 @@
+package com.meddle.bloggdUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggdUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggdUpApplication.class, args);
+	}
+
+}
